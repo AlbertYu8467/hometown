9fedd63252430e2b390921bd115e769fd6249da5
yuhu2017 <846788588@qq.com>
2018-03-28 11:34:23 +0800

    change layout

--- my-home/src/components/Home.jsx
+++ my-home/src/components/Home.jsx
@@ -11,8 +11,25 @@ class Home extends Component {
             <React.Fragment>
                 <canvas id="canvas" width='100%' height='100%'></canvas>
                 <div  className='home'>
-                    <div className='logo'></div>
-                    <div className='motto'>书山有路勤为径 学海无涯苦作舟</div>
+                    <div className='logo'>
+                        <ul className="cube-inner">
+                            <li className="front"></li>
+                            <li className="back"></li>
+                            <li className="right"></li>
+                            <li className="left"></li>
+                            <li className="top"></li>
+                            <li className="bottom"></li>
+                        </ul>
+                        <ul className="cube">
+                            <li className="front"></li>
+                            <li className="back"></li>
+                            <li className="right"></li>
+                            <li className="left"></li>
+                            <li className="top"></li>
+                            <li className="bottom"></li>
+                        </ul>
+                    </div>
+                    <div className='motto'>書山有路勤爲徑 學海無涯苦作舟</div>
                 </div>
             </React.Fragment>
         )
