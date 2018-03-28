9c2e86ee35036fddb72b0b074e5c351a5bd5fcf3
yuhu2017 <846788588@qq.com>
2018-03-28 15:38:42 +0800

    poem component

--- my-home/src/components/App.jsx
+++ my-home/src/components/App.jsx
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import { Link, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Photo from './Photo';
+import Poem from './Poem';
 
 class App extends Component {
     render(){
@@ -12,13 +13,14 @@ class App extends Component {
                 <div className='container'>
                     <Switch>
                     <Route exact path="/" component={Home}/>
-                    <Route exact path="/photo" component={Photo}/>
+                    <Route  path="/photo" component={Photo}/>
+                    <Route  path="/poem" component={Poem}/>
                 </Switch>
                 </div>
                 <ul className='nav'>
                     <li className='nav-label'><Link to="/">首页</Link></li>
                     <li className='nav-label'><Link to="/photo">相册</Link></li>
-                    <li className='nav-label'><Link to="/">小诗</Link></li>
+                    <li className='nav-label'><Link to="/poem">小诗</Link></li>
                     <li className='nav-label'><Link to="/">音乐</Link></li>
                     <li className='nav-label'><Link to="/">组件</Link></li>
                 </ul>
